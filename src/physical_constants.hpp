fd08ddebf6ae822345db7d76be2e7a224afd08d9
jasonxia17 <jasonx3@illinois.edu>
2019-04-21 17:29:06 -0500

    Increased radii of star and planet

--- src/physical_constants.hpp
+++ src/physical_constants.hpp
@@ -2,8 +2,8 @@
 
 namespace physicsvisuals {
 
-const double kStarRadius = 0.25;
-const double kPlanetRadius = 0.1;
+const double kStarRadius = 0.35;
+const double kPlanetRadius = 0.15;
 const double kGravitationalParameter = 16;  // the quantity GM
 
 }  // namespace physicsvisuals
