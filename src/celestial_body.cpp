9d357620338f48e722dda44b53ab18019de8c4b9
jasonxia17 <jasonx3@illinois.edu>
2019-04-21 00:30:29 -0500

    Added a position log to CelestialBody to store past positions

--- src/celestial_body.cpp
+++ src/celestial_body.cpp
@@ -6,14 +6,23 @@ const vec2& CelestialBody::getPosition() const {
     return position_;
 }
 
+const std::deque<vec2>& CelestialBody::getPositionLog() const {
+    return position_log_;
+}
+
 void CelestialBody::updateVelocityAndPosition(double time_step) {
-    const double kGravitationalParameter = 10; // the quantity GM
+    const double kGravitationalParameter = 16; // the quantity GM
 
     vec2 acceleration = -kGravitationalParameter / pow(glm::length(position_), 3) * position_;
     // a = -GM / |r|^3 * r, where r is a vector
 
     velocity_ += acceleration * time_step;  // dv = a * dt
     position_ += velocity_ * time_step;     // dx = v * dt
+
+    position_log_.push_front(position_);
+    if (position_log_.size() > kMaxPositionsStored) {
+        position_log_.pop_back();
+    }
 }
 
 }  // namespace physicsvisuals
--- src/celestial_body.hpp
+++ src/celestial_body.hpp
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <deque>
+
 #include "ofMain.h"
 
 using glm::vec2;
@@ -6,27 +9,41 @@ using glm::vec2;
 namespace physicsvisuals {
 
 class CelestialBody {
-
 public:
     /**
      * Parameters are hard-coded for now, will have legit constructor later.
      */
     CelestialBody() {
-        position_.x = 0.5;
-        velocity_.y = 6;
+        position_.x = 4;
+        velocity_.y = 1.3;
     }
 
     const vec2& getPosition() const;
+    const std::deque<vec2>& getPositionLog() const;
 
     /**
      * Performs one step of Euler's method to calculate the new velocity and position of the planet.
      * The parameter time_step specifies the step size.
+     * 
+     * Also updates position_log_.
      */
     void updateVelocityAndPosition(double time_step);
+
+    static const size_t kMaxPositionsStored = 1000;
 
 private:
     vec2 position_;
     vec2 velocity_;
+
+    /**
+     * Stores the past positions of the planet with the most recent
+     * positions first, for the purpose of drawing a trail. The number of
+     * positions stored is limited by kMaxPositionsStored; when this size is exceeded
+     * the oldest positions will be discarded from the back of the list. This
+     * prevents the class from storing too much unneeded information.
+     * 
+     */
+    std::deque<vec2> position_log_;
 };
 
 }  // namespace physicsvisuals
