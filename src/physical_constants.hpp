24d54c968fd1a2dc5e3d02515e71892dcac7c1b7
jasonxia17 <jasonx3@illinois.edu>
2019-04-23 00:02:56 -0500

    Removed unnecessary physical_constants.hpp file

--- src/physical_constants.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace physicsvisuals {
-
-const double kStarRadius = 0.35;
-const double kPlanetRadius = 0.15;
-const double kGravitationalParameter = 16;  // the quantity GM
-
-}  // namespace physicsvisuals
